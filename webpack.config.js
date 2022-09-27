ba8b39078621af0821865223567748d22d9a21fa
Dan Kotov <dkotov@ryerson.ca>
2022-09-27 16:03:36 -0400

    Setting up webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'prod' ? 'production' : 'development',
+  devtool: process.env.NODE_ENV === 'prod' ? false : 'source-map',
+  entry: {
+    content: path.resolve(__dirname, './src/content/index.js'),
+    background: path.resolve(__dirname, './src/background/index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, './src/manifest.json'),
+          to: path.resolve(__dirname, 'dist'),
+        }
+      ]
+    })
+  ]
+}
