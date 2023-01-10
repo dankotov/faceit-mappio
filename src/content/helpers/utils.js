2fcabd83b3dba67c4e3b37bf18fe70e950ebbc69
Dan Kotov <dkotov@ryerson.ca>
2023-01-10 12:16:09 -0500

    Custom debounce function

--- src/content/helpers/utils.js
+++ src/content/helpers/utils.js
@@ -1,6 +1,17 @@
-export const elementExistsIn = (selectorString, parent) => {
-  if (parent === null) return false
+export const debounce = (cb, delay = 200) => {
+  let timeout;
 
-  const exists = parent.querySelector(selectorString) === null ? false : true
-  return exists
-}
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      cb(...args);
+    }, delay);
+  };
+};
+
+export const elementExistsIn = (selectorString, parent) => {
+  if (parent === null) return false;
+
+  const exists = parent.querySelector(selectorString) === null ? false : true;
+  return exists;
+};
