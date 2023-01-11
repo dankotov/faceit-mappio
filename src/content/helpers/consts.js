949bc1b87eca7170e0458d0fe3e8444950c9613c
Dan Kotov <dkotov@ryerson.ca>
2023-01-11 12:49:24 -0500

    Changing active map pool labels

--- src/content/helpers/consts.js
+++ src/content/helpers/consts.js
@@ -1,15 +1,15 @@
 export const FACEIT_API_BASE_URL = "https://open.faceit.com";
 export const FACEIT_API_BEARER_TOKEN = "e5116e15-8d53-445e-bf01-145a7f04129c";
 
-export const activeMapPool = {
-  de_dust2: "DUS",
-  de_inferno: "INF",
-  de_ancient: "ANC",
-  de_overpass: "OVE",
-  de_mirage: "MIR",
-  de_nuke: "NUK",
-  de_vertigo: "VER",
-  de_anubis: "ANU",
-};
-
 export const CACHE_TIME = 1000 * 60 * 15;
+
+export const activeMapPool = {
+  de_dust2: "Dust2",
+  de_inferno: "Inferno",
+  de_ancient: "Ancient",
+  de_overpass: "Overpass",
+  de_mirage: "Mirage",
+  de_nuke: "Nuke",
+  de_vertigo: "Vertigo",
+  de_anubis: "Anubis",
+};
