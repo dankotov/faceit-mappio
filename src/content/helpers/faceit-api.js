5ec186f19dab46b5259037c63e3fed5d1dce1d6f
Dan Kotov <dkotov@ryerson.ca>
2023-01-23 13:29:36 -0500

    Add conditional authorization header to faceit api requests, support for 2 base api urls. Update method calls to pass new required arguments. Add methods to get captain players' ids and fetch captain matches veto history

--- src/content/helpers/faceit-api.js
+++ src/content/helpers/faceit-api.js
@@ -1,29 +1,36 @@
 import pMemoize from "p-memoize";
 import {
   CACHE_TIME,
-  FACEIT_API_BASE_URL,
+  FACEIT_OPEN_BASE_URL,
   FACEIT_API_BEARER_TOKEN,
+  FACEIT_API_BASE_URL,
 } from "./consts";
 import { isRelevantMapStat } from "./utils";
 
-const fetchFaceitApi = async (requestPath) => {
-  console.log("called api", requestPath);
-  const response = await fetch(FACEIT_API_BASE_URL + requestPath, {
+const fetchFaceitApi = async (baseUrl, requestPath, auth = true) => {
+  console.log("API call:", baseUrl + requestPath);
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  if (auth) headers.Authorization = `Bearer ${FACEIT_API_BEARER_TOKEN}`;
+  const response = await fetch(baseUrl + requestPath, {
     method: "GET",
-    headers: {
-      Authorization: `Bearer ${FACEIT_API_BEARER_TOKEN}`,
-      "Content-Type": "application/json",
-    },
+    headers,
   });
-
   const json = await response.json();
   return json;
 };
 
-const fetchFaceitApiMemoized = pMemoize(fetchFaceitApi, { maxAge: CACHE_TIME });
+const fetchFaceitApiMemoized = pMemoize(fetchFaceitApi, {
+  maxAge: CACHE_TIME,
+  cacheKey: (arguments_) => JSON.stringify(arguments_),
+});
 
-const fetchMatchDetails = async (matchroomId) =>
-  await fetchFaceitApiMemoized(`/data/v4/matches/${matchroomId}`);
+export const fetchMatchDetails = async (matchroomId) =>
+  await fetchFaceitApiMemoized(
+    FACEIT_OPEN_BASE_URL,
+    `/data/v4/matches/${matchroomId}`
+  );
 
 const getPlayers = (matchDetails) => {
   const players = {};
@@ -36,22 +43,40 @@ const getPlayers = (matchDetails) => {
   return players;
 };
 
+const getCaptainsIds = (matchDetails) => {
+  return [
+    matchDetails.teams.faction1.leader,
+    matchDetails.teams.faction2.leader,
+  ];
+};
+
+const aggregatePlayerStats = async (nickname, playerId) => {
+  const playerStats = await fetchFaceitApiMemoized(
+    FACEIT_OPEN_BASE_URL,
+    `/data/v4/players/${playerId}/stats/csgo`
+  );
+  const stats = { nickname, id: playerId, maps: new Map([]) };
+  playerStats.segments.filter(isRelevantMapStat).forEach((map) => {
+    const data = {
+      games: map.stats["Matches"],
+      kd: map.stats["Average K/D Ratio"],
+      wr: map.stats["Win Rate %"],
+    };
+    stats.maps.set(map.label, data);
+  });
+  return stats;
+};
+
 const fetchPlayerDetails = async (matchroomId) => {
   const matchDetails = await fetchMatchDetails(matchroomId);
   const players = getPlayers(matchDetails);
 
-  Object.keys(players).forEach(async (nickname) => {
-    const playerStats = await fetchFaceitApiMemoized(
-      `/data/v4/players/${players[nickname].id}/stats/csgo`
-    );
-    playerStats.segments.filter(isRelevantMapStat).forEach((map) => {
-      const data = {
-        games: map.stats["Matches"],
-        kd: map.stats["Average K/D Ratio"],
-        wr: map.stats["Win Rate %"],
-      };
-      players[nickname].maps.set(map.label, data);
-    });
+  const playerStatsPromises = Object.keys(players).map(async (nickname) => {
+    return await aggregatePlayerStats(nickname, players[nickname].id);
+  });
+  const playerStats = await Promise.all(playerStatsPromises);
+  playerStats.forEach((playerStat) => {
+    players[playerStat.nickname].maps = playerStat.maps;
   });
 
   return players;
@@ -60,3 +85,37 @@ const fetchPlayerDetails = async (matchroomId) => {
 export const fetchMemoizedPlayerDetails = pMemoize(fetchPlayerDetails, {
   maxAge: CACHE_TIME,
 });
+
+const getPlayerMatches = async (playerId, i) => {
+  const m = await fetchFaceitApiMemoized(
+    FACEIT_OPEN_BASE_URL,
+    `/data/v4/players/${playerId}/history?game=csgo&limit=100&offset=${i * 100}`
+  );
+  return m;
+};
+
+export const fetchPlayerMatchList = async (playerId) => {
+  const increments = [...Array(3).keys()];
+  const matchPromises = increments.map(async (increment) => {
+    return await getPlayerMatches(playerId, increment);
+  });
+  const matches = (await Promise.all(matchPromises)).reduce(
+    (acc, curr) => acc.concat(curr.items),
+    []
+  );
+  return matches;
+};
+
+export const fetchMatchVetoDetails = async (matchroomId) =>
+  await fetchFaceitApiMemoized(
+    FACEIT_API_BASE_URL,
+    `/democracy/v1/match/${matchroomId}/history`,
+    false
+  );
+
+export const getCaptains = async (matchroomId) => {
+  const md = await fetchMatchDetails(matchroomId);
+  const captains = getCaptainsIds(md);
+
+  return captains;
+};
