6c55dc860a29dcbcc52b546375a2895b43f1317f
Dan Kotov <dkotov@ryerson.ca>
2023-01-23 13:39:57 -0500

    Remove unused method from add player map stats feature file

--- src/content/features/addPlayerMapStats.js
+++ src/content/features/addPlayerMapStats.js
@@ -4,16 +4,6 @@ import { getMatchroomPlayers, getNickname } from "../helpers/matchroom";
 import createMapStatsElement from "../components/mapStats";
 import { hasMappio } from "../helpers/utils";
 
-export const addPlayerMapStats = async (matchroomId) => {
-  const playerElements = getMatchroomPlayers();
-  const playerMapStats = await fetchMemoizedPlayerDetails(matchroomId);
-
-  playerElements.forEach((playerElement) => {
-    const nickname = getNickname(playerElement);
-    console.log(nickname);
-  });
-};
-
 export const debounceAddPlayerMapStats = debounce(async (matchroomId) => {
   const playerElements = getMatchroomPlayers();
   const playerMapStats = await fetchMemoizedPlayerDetails(matchroomId);
