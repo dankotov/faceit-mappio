ed3e89cf19d92c79a91ce8f87561c20c408d6aad
Dan Kotov <dkotov@ryerson.ca>
2023-01-25 18:15:42 -0500

    Add comments to utils helper methods

--- src/content/helpers/utils.js
+++ src/content/helpers/utils.js
@@ -1,16 +1,41 @@
 import { ACTIVE_MAP_POOL, ESCL } from "./consts";
 
+/**
+ * Checks whether an element identifiable by a query selector string in an HTML element.
+ *
+ * @param {string} [selectorString] Query selector string.
+ * @param {HTMLElement} [parent] HTML element to apply the `selectorString` to.
+ * @returns {boolean} Boolean that represents whether there is a querySelector match for `selectorString` element in the `parent` element
+ */
 export const elementExistsIn = (selectorString, parent) => {
   if (parent === null) return false;
 
   return parent.querySelector(selectorString) !== null;
 };
 
+/**
+ * Checks whether a map stat is eligible to be accounted for by the extension.
+ * @param {Object} [mapStat] Map stat object.
+ * @returns {boolean} Boolean that represents whether the map stat is eligible to be account for by the extension.
+ */
 export const isRelevantMapStat = (mapStat) =>
   mapStat.type === "Map" &&
   mapStat.mode === "5v5" &&
   ACTIVE_MAP_POOL.has(mapStat.label);
 
+/**
+ * Checks whether the provided element has any mappio extension related elements appended to it.
+ * @param {HTMLElement} element The HTML element.
+ * @returns {boolean} Boolean that represents whether the provided `element` has any mappio extension related elements appended to it.
+ */
+export const hasMappio = (element) =>
+  element.querySelector(`.${ESCL}`) !== null;
+
+/**
+ * Return a CSS color string that should be applied to the provided stat.
+ * @param {Object} [stat] Stat object.
+ * @returns {string} String that is a CSS color string.
+ */
 export const colorCodeStat = (stat) => {
   const colors = {
     red: "#d94949",
@@ -23,6 +48,3 @@ export const colorCodeStat = (stat) => {
 
   return kd >= 1.2 ? colors.green : colors.red;
 };
-
-export const hasMappio = (element) =>
-  element.querySelector(`.${ESCL}`) !== null;
