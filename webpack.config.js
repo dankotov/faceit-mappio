8bb65dcf0bebbea5fc326467e357fe93d4421a64
Dan Kotov <dkotov@ryerson.ca>
2023-01-25 19:22:18 -0500

    Add extension icons

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,10 @@ module.exports = {
           from: path.resolve(__dirname, "./src/manifest.json"),
           to: path.resolve(__dirname, "dist"),
         },
+        {
+          from: path.resolve(__dirname, "./src/icons/"),
+          to: path.resolve(__dirname, "dist/icons"),
+        },
       ],
     }),
   ],
