98b149669efbcd66e719f685e3e2d1b837b9b23b
Dan Kotov <dkotov@ryerson.ca>
2023-02-01 11:11:29 -0500

    Clean up default settings constant and setter method

--- src/background/index.js
+++ src/background/index.js
@@ -2,11 +2,6 @@ import { DEFAULT_SETTINGS } from "../shared/settings";
 
 chrome.runtime.onInstalled.addListener(() => {
   DEFAULT_SETTINGS.forEach((value, key) => {
-    console.log("setting here", key, value);
-    chrome.storage.local.set({ [key]: value }).then(() => {
-      chrome.storage.local.get(key).then((result) => {
-        console.log("getting here: ", result[key]);
-      });
-    });
+    chrome.storage.local.set({ [key]: value });
   });
 });
--- src/shared/settings.ts
+++ src/shared/settings.ts
@@ -1,8 +1,3 @@
-// export interface BaseSetting {
-//   enabled: boolean;
-// }
-
 export const DEFAULT_SETTINGS = new Map<string, boolean>([
   ["showPlayerMapsStats", true],
-  ["test", false],
 ]);
