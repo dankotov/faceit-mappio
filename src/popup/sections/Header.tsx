18cc41d30b005d9cf06df217519b00a087b95234
Dan Kotov <dkotov@ryerson.ca>
2023-02-02 15:29:02 -0500

    Move and rename HeaderPopup from components to sections as Popup

---
+++ src/popup/sections/Header.tsx
@@ -0,0 +1,8 @@
+const Header = () => (
+  <header className="popup-header">
+    <img src="./icons/iconBgLessVector.svg" alt="FACEIT Mappio Logo" />
+    <h1>FACEIT Mappio</h1>
+  </header>
+);
+
+export default Header;
