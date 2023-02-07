54e068dc123eee5104f8be57ce59d55f8827e5cc
Dan Kotov <dkotov@ryerson.ca>
2023-02-07 12:28:40 -0500

    refactor: move popup header styles to its tsx file

--- src/popup/sections/Header.tsx
+++ src/popup/sections/Header.tsx
@@ -1,8 +1,30 @@
 const Header = () => (
-  <header className="popup-header">
-    <img src="./icons/iconBgLessVector.svg" alt="FACEIT Mappio Logo" />
-    <h1>FACEIT Mappio</h1>
-  </header>
+  <>
+    <header>
+      <img src="./icons/iconBgLessVector.svg" alt="FACEIT Mappio Logo" />
+      <h1>FACEIT Mappio</h1>
+    </header>
+
+    <style jsx>{`
+      header {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 15px 0px;
+        border-bottom: 1px solid #303030;
+      }
+
+      header > img {
+        min-height: 48px;
+        height: 48px;
+      }
+
+      header > h1 {
+        margin-top: 10px;
+      }
+    `}</style>
+  </>
 );
 
 export default Header;
