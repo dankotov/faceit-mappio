41537cc9b11c99ba77c111d9e274b72e78bb21e9
Dan Kotov <dkotov@ryerson.ca>
2023-02-07 12:49:06 -0500

    refactor: move section description element styles to its tsx file

--- src/popup/components/SectionDescription.tsx
+++ src/popup/components/SectionDescription.tsx
@@ -1,5 +1,14 @@
 const SectionDescription = ({ description }: { description: string }) => (
-  <p className="section-description">{description}</p>
+  <>
+    <p>{description}</p>
+
+    <style jsx>{`
+      p {
+        color: #a0a0a0;
+        margin-bottom: 8px;
+      }
+    `}</style>
+  </>
 );
 
 export default SectionDescription;
