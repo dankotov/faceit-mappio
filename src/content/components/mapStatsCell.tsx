211e15db62b59a01337a798fbf6b5aab5cd6a8f6
Dan Kotov <dkotov@ryerson.ca>
2023-02-10 13:22:51 -0500

    refactor: increase game number stat element width
    Expand game number stat element width to accomodate for player's with
    many played matches.

--- src/content/components/mapStatsCell.tsx
+++ src/content/components/mapStatsCell.tsx
@@ -15,7 +15,7 @@ const mapStatsCell = ({ label = "ERR", stat = { games: "0", kd: "0" } }) => (
     }}
   >
     <div style={{ width: 22 }}>{label}</div>
-    <div style={{ width: 26 }}>{stat.games}</div>
+    <div style={{ width: 30 }}>{stat.games}</div>
     <div style={{ width: 22 }}>{stat.kd}</div>
   </div>
 );
