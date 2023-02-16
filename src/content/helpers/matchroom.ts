5d7dea81e453e0926bc2d3358811b2f12409267c
Dan Kotov <dkotov@ryerson.ca>
2023-02-15 20:38:17 -0500

    refactor: remove remaining JSDoc annotations
    When parts of the project were written in JS, I added JSDoc annotations
    to make the forthcoming transition to TS easier. Now that the transition
    is done and everything is typed with TS, I am removing the JSDoc
    annotations.

--- src/content/helpers/matchroom.ts
+++ src/content/helpers/matchroom.ts
@@ -2,24 +2,18 @@ import { elementExistsIn } from "./utils";
 
 /**
  * Gets the current page's shadow root element.
- *
- * @returns {HTMLDivElement} The matchroom's shadow root element.
  */
 const getShadowRootElement = () =>
   document.querySelector("#parasite-container")?.shadowRoot;
 
 /**
  * Checks if the current page has a main content element.
- *
- * @returns {boolean} Boolean that represents whether the current page has a main content element.
  */
 export const hasMainContentElement = () =>
   !!document.querySelector("#main-content");
 
 /**
  * Checks if current page is the matchroom page.
- *
- * @returns {boolean} Boolean that represents whether the current page is the matchroom page.
  */
 export const isMatchroomPage = () => {
   const pageUrl = document.location.href;
@@ -28,23 +22,17 @@ export const isMatchroomPage = () => {
 
 /**
  * Checks if the current page's shadow root element is loaded.
- *
- * @returns {boolean} Boolean that represents whether the current page's shadow root is loaded.
  */
 export const isShadowRootLoaded = () => !!getShadowRootElement();
 
 /**
  * Checks if the current page's matchroom overview element is loaded.
- *
- * @returns {boolean} Boolean that represents whether the current page's matchroom overview element is loaded.
  */
 export const isMatchroomOverviewLoaded = () =>
   !!getShadowRootElement()?.querySelector("#MATCHROOM-OVERVIEW");
 
 /**
  * Checks if the current page's roster lists elements are loaded.
- *
- * @returns {boolean} Boolean that represents whether the current page's roster lists elements are loaded.
  */
 export const rosterListsLoaded = () => {
   const sr = getShadowRootElement();
@@ -57,7 +45,6 @@ export const rosterListsLoaded = () => {
 
 /**
  * Gets the current matchroom's ID from the URL
- * @returns {string} The current matchroom's ID
  */
 export const getMatchroomId = (): string => {
   const pageUrl = document.location.href;
@@ -70,9 +57,6 @@ export const getMatchroomId = (): string => {
 
 /**
  * Returns an array of individual matchroom player card HTML elements (compattible /w vanilla and faceit-enhacner layouts).
- *
- * @param {HTMLDivElement} [rosterContainer] HTML container element that holds the team member elements.
- * @returns {Array.<HTMLDivElement>} Array of indiviual player card HTML elements.
  */
 const getRosterList = (rosterContainer: HTMLDivElement) => {
   const roster: HTMLDivElement[] = [];
@@ -112,8 +96,6 @@ const getRosterList = (rosterContainer: HTMLDivElement) => {
 
 /**
  * Gets the HTML player card elements taht represent all players in the matchroom.
- *
- * @return {Array.<HTMLDivElement>} Array of HTML elements that represent all players' player cards in the matchroom.
  */
 export const getMatchroomPlayers = () => {
   const mo = getShadowRootElement()?.querySelector("#MATCHROOM-OVERVIEW");
@@ -133,8 +115,6 @@ export const getMatchroomPlayers = () => {
 
 /**
  * Gets the HTML player card elements that represent the captains of each team in the matchroom.
- *
- * @returns {Array.<HTMLDivElement>} Array of HTML elements that represent the captains of each team in the matchroom.
  */
 export const getCaptainElements = () => {
   const players = getMatchroomPlayers();
@@ -144,8 +124,6 @@ export const getCaptainElements = () => {
 
 /**
  * Gets the HTML element that represents the info section of the matchroom.
- *
- * @returns {HTMLDivElement} HTML element that represents the info section of the matchroom.
  */
 export const getInfoElement = () => {
   const mo = getShadowRootElement()?.querySelector("#MATCHROOM-OVERVIEW");
@@ -155,9 +133,6 @@ export const getInfoElement = () => {
 
 /**
  * Gets a player's nickname by parsing their matchroom HTML player card element (compattible /w vanilla and faceit-enhancer layouts).
- *
- * @param {HTMLDivElement} [playerCard] The player's HTML player card element.
- * @returns {string} The player's nickame.
  */
 export const getNickname = (playerCard: HTMLDivElement) =>
   (
--- src/content/helpers/utils.ts
+++ src/content/helpers/utils.ts
@@ -4,11 +4,7 @@ import { MapCodename } from "../../shared/types/csgo-maps";
 import { SegmentStats, Stats } from "../../shared/types/stats";
 
 /**
- * Checks whether an element identifiable by a query selector string in an HTML element.
- *
- * @param {string} [selectorString] Query selector string.
- * @param {HTMLElement} [parent] HTML element to apply the `selectorString` to.
- * @returns {boolean} Boolean that represents whether there is a querySelector match for `selectorString` element in the `parent` element
+ * Checks whether an element identifiable by a query selector string is in an HTML element.
  */
 export const elementExistsIn = (
   selectorString: string,
@@ -17,8 +13,6 @@ export const elementExistsIn = (
 
 /**
  * Checks whether a map stat is eligible to be accounted for by the extension.
- * @param {Object} [mapStat] Map stat object.
- * @returns {boolean} Boolean that represents whether the map stat is eligible to be account for by the extension.
  */
 export const isRelevantMapStat = (segmentStats: SegmentStats) =>
   segmentStats.type === "Map" &&
@@ -27,16 +21,12 @@ export const isRelevantMapStat = (segmentStats: SegmentStats) =>
 
 /**
  * Checks whether the provided element has any mappio extension related elements appended to it.
- * @param {HTMLElement} element The HTML element.
- * @returns {boolean} Boolean that represents whether the provided `element` has any mappio extension related elements appended to it.
  */
 export const hasMappio = (element: HTMLElement) =>
   !!element.querySelector(`.${ESCL}`);
 
 /**
  * Return a CSS color string that should be applied to the provided stat.
- * @param {Object} [stat] Stat object.
- * @returns {string} String that is a CSS color string.
  */
 export const colorCodeStat = (stats: Stats) => {
   const [games, kd] = [Number(stats.games), Number(stats.kd)];
