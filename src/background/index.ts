ed4f448e51327ee1becc65db5cfaf90c21965de8
Dan Kotov <dkotov@ryerson.ca>
2023-02-20 16:19:08 -0500

    refactor: inherit existing local setting on update
    Check chrome local storage for existing extension settings and use
    those after updates. This way when extension is updated, users do not
    have their settings reset to default.

--- src/background/index.ts
+++ src/background/index.ts
@@ -2,6 +2,14 @@ import { DEFAULT_SETTINGS } from "../shared/settings";
 
 chrome.runtime.onInstalled.addListener(() => {
   DEFAULT_SETTINGS.forEach(async (value, featureName) => {
+    // get current feature setting
+    const featureOldSetting = (await chrome.storage.local.get(featureName))[
+      featureName
+    ];
+    // if feature setting exists -> do nothing
+    if (featureOldSetting !== undefined) return;
+
+    // feature setting doesnt exist -> set default
     chrome.storage.local.set({ [featureName]: value });
   });
 });
