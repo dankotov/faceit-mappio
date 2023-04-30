565a45b6931c6add1d2c42188e41ae856b8c4272
Dan Kotov <dkotov@ryerson.ca>
2023-04-29 23:39:28 -0400

    refactor: use appropriate param name in a util fn

--- src/content/helpers/utils.ts
+++ src/content/helpers/utils.ts
@@ -27,8 +27,8 @@ export const colorCodeStat = (stats: Stats) => {
   return kd >= 1.2 ? colors.faceitgreen : colors.faceitred;
 };
 
-export const padZero = (smth: number) => {
-  return smth.toString().padStart(2, "0");
+export const padZero = (toBePadded: number) => {
+  return toBePadded.toString().padStart(2, "0");
 };
 
 export const getFaceitTimestamp = (date: Date, yearOffset: number = 0) => {
