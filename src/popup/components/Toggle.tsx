dc2c23544c343448f2e596549e182bae279ec8d5
Dan Kotov <dkotov@ryerson.ca>
2023-07-18 13:22:48 -0400

    refactor(eslint): enforce arrow body style

--- src/popup/components/Toggle.tsx
+++ src/popup/components/Toggle.tsx
@@ -6,67 +6,65 @@ const Toggle = ({
 }: {
   isToggled: boolean;
   onToggle: () => void;
-}) => {
-  return (
-    <>
-      <label>
-        <input type="checkbox" checked={isToggled} onChange={onToggle} />
-        <span className="slider" />
-      </label>
+}) => (
+  <>
+    <label>
+      <input type="checkbox" checked={isToggled} onChange={onToggle} />
+      <span className="slider" />
+    </label>
 
-      <style jsx>{`
-        label {
-          position: relative;
-          display: inline-block;
-          width: 55px;
-          height: 24px;
-        }
+    <style jsx>{`
+      label {
+        position: relative;
+        display: inline-block;
+        width: 55px;
+        height: 24px;
+      }
 
-        label > input {
-          opacity: 0;
-          width: 0;
-          height: 0;
-        }
+      label > input {
+        opacity: 0;
+        width: 0;
+        height: 0;
+      }
 
-        .slider {
-          position: absolute;
-          cursor: pointer;
+      .slider {
+        position: absolute;
+        cursor: pointer;
 
-          top: 0;
-          left: 0;
-          right: 0;
-          bottom: 0;
-          border-radius: 23px;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        border-radius: 23px;
 
-          background-color: ${colors.backgrey};
-          transition: 0.4s ease-in-out;
-        }
+        background-color: ${colors.backgrey};
+        transition: 0.4s ease-in-out;
+      }
 
-        .slider::before {
-          box-sizing: border-box;
-          position: absolute;
-          content: "";
+      .slider::before {
+        box-sizing: border-box;
+        position: absolute;
+        content: "";
 
-          height: 17px;
-          width: 17px;
-          top: 50%;
-          transform: translate(6px, -50%);
-          border-radius: 50%;
+        height: 17px;
+        width: 17px;
+        top: 50%;
+        transform: translate(6px, -50%);
+        border-radius: 50%;
 
-          background-color: ${colors.forewhite};
-          transition: 0.4s ease-in-out;
-        }
+        background-color: ${colors.forewhite};
+        transition: 0.4s ease-in-out;
+      }
 
-        input:checked + .slider {
-          background-color: ${colors.faceitgreen};
-        }
-        input:checked + .slider::before {
-          top: 50%;
-          transform: translate(33px, -50%);
-        }
-      `}</style>
-    </>
-  );
-};
+      input:checked + .slider {
+        background-color: ${colors.faceitgreen};
+      }
+      input:checked + .slider::before {
+        top: 50%;
+        transform: translate(33px, -50%);
+      }
+    `}</style>
+  </>
+);
 
 export default Toggle;
